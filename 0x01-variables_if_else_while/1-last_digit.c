4424d48f8a20919975e2c416cdcc6ebbadbc52cf
Bukasson <chukwuebukaprosperous#gmail.com>
2023-07-17 19:39:50 -0700

    Got to know place value

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>=0)
-		a=abs(n%10);
-	else 
-		a=-(abs(n%10));
+	if (n >= 0)
+		a = abs(n % 10);
+	else
+		a = -(abs(n % 10));
 
 	printf("Last digit of %d is %d ", n, a);
 
