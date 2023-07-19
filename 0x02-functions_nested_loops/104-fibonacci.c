0a2f951087ac05eb53ca926cc56f0bfc0a26eaf4
Bukasson <chukwuebukaprosperous#gmail.com>
2023-07-18 18:07:11 -0700

    fibonacci 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
-#define MAX_DIGITS 2000
+#define MAX_DIGITS 500
 
-void addStrings(char *num1, char *num2, char *result) {
+void addArrays(int *num1, int *num2, int *result) {
 int carry = 0;
-int len1 = strlen(num1);
-int len2 = strlen(num2);
-int maxLen = (len1 > len2) ? len1 : len2;
-for (int i = 0; i < maxLen; i++) {
-int digit1 = (i < len1) ? (num1[len1 - 1 - i] - '0') : 0;
-int digit2 = (i < len2) ? (num2[len2 - 1 - i] - '0') : 0;
-int sum = digit1 + digit2 + carry;
+for (int i = 0; i < MAX_DIGITS; i++) {
+int sum = num1[i] + num2[i] + carry;
+result[i] = sum % 10;
 carry = sum / 10;
-result[maxLen - 1 - i] = (sum % 10) + '0';
 }
-if (carry)
-result[0] = carry + '0';
 }
+
 /**
  * main - Entry point of the program
  *
@@ -25,22 +18,44 @@ result[0] = carry + '0';
  */
 int main(void)
 {
-char fib1[MAX_DIGITS], fib2[MAX_DIGITS], next_fib[MAX_DIGITS];
-memset(fib1, 0, sizeof(fib1));
-memset(fib2, 0, sizeof(fib2));
-memset(next_fib, 0, sizeof(next_fib));
-strcpy(fib1, "1");
-strcpy(fib2, "2");
-printf("%s, %s, ", fib1, fib2);
+int fib1[MAX_DIGITS] = {0}, fib2[MAX_DIGITS] = {0}, next_fib[MAX_DIGITS] = {0};
+
+fib1[MAX_DIGITS - 1] = 1;
+fib2[MAX_DIGITS - 1] = 2;
+
+printf("%d", fib1[MAX_DIGITS - 1]);
+for (int i = MAX_DIGITS - 2; i >= 0; i--)
+printf("%d", fib1[i]);
+printf(", %d", fib2[MAX_DIGITS - 1]);
+for (int i = MAX_DIGITS - 2; i >= 0; i--)
+printf("%d", fib2[i]);
+printf(", ");
+
 for (int fib_count = 3; fib_count <= 98; fib_count++)
 {
-addStrings(fib1, fib2, next_fib);
-strcpy(fib1, fib2);
-strcpy(fib2, next_fib);
-if (fib_count != 98)
-printf("%s, ", next_fib);
-else
-printf("%s\n", next_fib);
+addArrays(fib1, fib2, next_fib);
+
+// Shift arrays for the next iteration
+for (int i = 0; i < MAX_DIGITS; i++) {
+fib1[i] = fib2[i];
+fib2[i] = next_fib[i];
 }
-return (0);
+
+// Find the first non-zero digit to start printing
+int start_printing = 0;
+while (start_printing < MAX_DIGITS && next_fib[start_printing] == 0)
+start_printing++;
+
+// Print the next Fibonacci number
+printf("%d", next_fib[MAX_DIGITS - 1 - start_printing]);
+for (int i = MAX_DIGITS - 2 - start_printing; i >= 0; i--)
+printf("%d", next_fib[i]);
+
+if (fib_count != 98)
+printf(", ");
+else
+printf("\n");
+}
+
+return 0;
 }
